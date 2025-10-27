a9676e08c28ffc7ede6964b8d6f7d9a7cbf96468
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2025-10-27 09:08:36 -0300

    Add files via upload

--- 5-RPC_Python/cliente.py
+++ 5-RPC_Python/cliente.py
@@ -1,9 +1,20 @@
 import xmlrpc.client, datetime
 
-servidor = xmlrpc.client.ServerProxy("http://localhost:8000/")
+servidor = xmlrpc.client.ServerProxy("http://10.103.16.19:8000/")
 
 hoje = servidor.hoje()
-
 # convert the ISO8601 string to a datetime object
 data_hora_convertida = datetime.datetime.strptime(hoje.value, "%Y%m%dT%H:%M:%S")
 print("Hoje é: %s" % data_hora_convertida.strftime("%d.%m.%Y, %H:%M:%S"))
+
+nome = 'Alexandre de Oliveira Zamberlan'
+email = servidor.gerar_email(nome)
+print(f'Email: {email}')
+
+
+frase = 'a turma de sistemas distribuídos da UFN é composta de alunos do sexo masculino. ' \
+'Os alunos são divididos em grupos: os que moram foram de Santa Maria; os que jogam volei; e os' \
+'bagaços... que não fazem os exercícios que o profe querido pediu com carinho.'
+
+frase_sem_artigos = servidor.retirar_artigos(frase)
+print(f'Frase sem artigos: ', frase_sem_artigos)
--- 5-RPC_Python/servidor.py
+++ 5-RPC_Python/servidor.py
@@ -5,7 +5,45 @@ def hoje_eh():
 	hoje = datetime.datetime.today()
 	return xmlrpc.client.DateTime(hoje)
 
-servidor = SimpleXMLRPCServer(("localhost", 8000))
-print("Ouvindo a porta 8000...")
-servidor.register_function(hoje_eh, "hoje")
+def gerar_email(nome):	
+   try:
+      nomes = nome.split()
+      email = nomes[0] + '.' + nomes[-1] + '@ufn.edu.br'
+      email = email.lower()
+      return email
+   except Exception as e:
+      print(f'Erro: {e}')
+      return None
+   
+def retirar_artigos(frase):
+    # Artigos definidos
+    artigos_definidos = ["o", "a", "os", "as"]
+
+    # Artigos indefinidos
+    artigos_indefinidos = ["um", "uma", "uns", "umas"]
+
+    # Todos juntos (opcional)
+    artigos = artigos_definidos + artigos_indefinidos
+
+    # converter tudo para minúsculas e dividir em palavras
+    palavras = frase.split()
+
+    # remover os artigos
+    palavras_sem_artigos = [p for p in palavras if p.lower() not in artigos]
+
+    # palavras_sem_artigos = []
+    # for p in palavras:
+    #     if p.lower() not in artigos:
+    #         palavras_sem_artigos.append(p)
+
+    # juntar de volta em uma string
+    frase_sem_artigos = " ".join(palavras_sem_artigos)
+
+    return frase_sem_artigos
+		
+servidor = SimpleXMLRPCServer(("localhost", 1099))
+print("Ouvindo a porta 1099...")
+servidor.register_function(hoje_eh, "hoje_eh")
+servidor.register_function(gerar_email, "gerar_email")
+servidor.register_function(retirar_artigos, "retirar_artigos")
 servidor.serve_forever()
